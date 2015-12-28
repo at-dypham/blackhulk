62c511c9b9a7428c30923ee50f5a59c59767210a
Vinh Nguyen <vinh.nglx@gmail.com>
2015-12-28 16:10:02 +0700

    Get posts API

---
+++ lib/github.rb
@@ -0,0 +1,20 @@
+class GitHub
+  include HTTParty
+
+  base_uri 'https://api.github.com'
+
+  attr_reader :options
+
+  def initialize
+    @options = { query: {access_token: ENV['personal_access_token']} }
+  end
+
+  def issues
+    response = self.class.get("/repos/#{ENV['github_user_name']}/#{ENV['github_repo']}/issues", options)
+    if response.code == 200
+      return JSON.parse(response.body)
+    else
+      return nil
+    end
+  end
+end
---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  describe "GET #index" do
+    it "returns http success" do
+    end
+  end
+end
