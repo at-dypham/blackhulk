042e50f1ff39d52d593ae65bad3a08bd1d6ba707
Vinh Nguyen <vinh.nglx@gmail.com>
2015-12-29 16:18:35 +0700

    Update tests for github_spec

--- spec/lib/github_spec.rb
+++ spec/lib/github_spec.rb
@@ -9,6 +9,8 @@ describe GitHub do
 
     it 'returns list issues' do
       expect(@issues).to be_an_instance_of(Array)
+      expect(@issues.first['title']).to eq 'Found a bug'
+      expect(@issues.first['body']).to eq "I'm having a problem with this."
     end
   end
 end
