83165cdf1fbc9137d33eb6a38d500316e83cc2b2
Vinh Nguyen <vinh.nglx@gmail.com>
2015-12-29 16:38:45 +0700

    Revert travis in saddler

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -4,9 +4,6 @@ set -v
 if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
               checkstyle_filter-git saddler saddler-reporter-github
-  gem install travis
-
-  travis encrypt -r FancyBlog/blackhulk "GITHUB_ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}"
 
   git diff -z --name-only origin/master \
    | xargs -0 rubocop-select \
