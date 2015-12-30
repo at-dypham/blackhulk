88b95853102dc3f695974d4e55581fdb12e5332d
Vinh Nguyen <vinh.nglx@gmail.com>
2015-12-30 23:31:40 +0700

    Solve issue #7, #8

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,11 +14,23 @@ RSpec.describe PostsController, type: :controller do
           to_return(:status => 400, :body => "", :headers => {})
       end
 
-      it 'returns http not_found' do
-        get :index
+      it 'returns http bad_request' do
+        get :index, format: :json
         expect(response.status).to eq 400
         expect(JSON.parse(response.body)['message']).to eq 'Please check your config'
       end
+    end
+  end
+
+  describe 'GET #show' do
+    it 'return http success' do
+      get :show, id: "found-a-bug"
+      expect(response.status).to eq 200
+    end
+
+    it 'returns http not_found' do
+      get :show, id: "bug"
+      expect(response.status).to eq 404
     end
   end
 end
