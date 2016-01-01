815ab0dd1b2fef83ed8b0200c20af77e8f55153f
Duy Khoa <duykhoa12t@gmail.com>
2016-01-01 15:59:18 +0700

    Update blog title & blog description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_blog_setting
+
+  private
+
+  def set_blog_setting
+    @blog_setting = {
+      title: ENV['blog_title'],
+      desc: ENV['blog_description']
+    }
+  end
 end
